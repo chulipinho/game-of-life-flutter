95a1604c52e9172fa54dd2165e6003cabf4849e3
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-01-11 12:00:11 -0300

    Implemented board functions

--- lib/board/board_controller.dart
+++ lib/board/board_controller.dart
@@ -7,28 +7,64 @@ class BoardController {
   BoardController(this.rows, this.columns);
 
   List<List<Cell>> boardData = [];
+  List<List<Cell>> backupBoardData = [];
 
-  List<Cell> aliveCells = [];
-
-  void _manageCellData(Cell cell) {
-    if (cell.isAlive){
-      aliveCells.add(cell);
-    }else if (aliveCells.contains(cell)){
-      aliveCells.remove(cell);
-    }
+  void createBackup() {
+    backupBoardData = boardData;
   }
-  
+
   void init() {
-    for (int x = 0; x < rows; x++){
-      for (int y = 0; y < columns; y++){
+    for (int x = 0; x < rows; x++) {
+      for (int y = 0; y < columns; y++) {
         boardData[x][y] = Cell(x, y);
-        boardData[x][y].state.addListener(() => _manageCellData(boardData[x][y]));
       }
     }
   }
-  
-  void runCycle(){
-    
+
+  bool isOutOfRange(int x, int y) {
+    if (x > 0 && x < rows && y > 0 && y < columns) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
+  int aliveNeighbors(Cell cell) {
+    int aliveNeighbours = 0;
+
+    for (int x = -1; x <= 1; x++) {
+      for (int y = -1; y <= 1; y++) {
+        int row = x + cell.row;
+        int col = y + cell.col;
+
+        if (backupBoardData[row][col].isAlive && !isOutOfRange(row, col)) {
+          aliveNeighbours++;
+        }
+      }
+    }
+    return aliveNeighbours;
+  }
+
+  void manageCellState(int neighbors, Cell cell) {
+    if (neighbors > 3 || neighbors < 2) {
+      if (cell.isAlive) {
+        cell.die();
+      }
+      return;
+    }
+    if (!cell.isAlive) {
+      cell.live();
+    }
+  }
+
+  void runCycle() {
+    createBackup();
+    for (List<Cell> cells in boardData) {
+      for (Cell cell in cells) {
+        int neighborsAlive = aliveNeighbors(cell);
+
+        manageCellState(neighborsAlive, cell);
+      }
+    }
+  }
 }
